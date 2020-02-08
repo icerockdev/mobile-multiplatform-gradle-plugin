1cdff911254f888f67d4aae9d7e251e119fb5346
Nicholas J Halase <nhalase@thumbcat.io>
2020-02-07 20:01:24 -0600

    Update ios target configuration with shortcut
    The `ios` shortcut creates targets for iosArm64 and iosX64 while also
    creating an intermediate source set of iosMain. Manually configuring
    iosArm64 and iosX64 is no longer required as of 1.3.60.
    
    Signed-off-by: Nicholas J Halase <nhalase@thumbcat.io>

--- src/main/kotlin/KotlinMultiplatformExtensionExt.kt
+++ src/main/kotlin/KotlinMultiplatformExtensionExt.kt
@@ -4,23 +4,20 @@
 
 import org.gradle.api.Project
 import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
-import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
 
 fun Project.setupFramework(
     exports: List<KotlinNativeExportable>
 ) {
-    val configureIos: KotlinNativeTarget.() -> Unit = {
-        binaries {
-            framework("MultiPlatformLibrary") {
-                freeCompilerArgs += "-Xobjc-generics"
-
-                exports.forEach { it.export(project, this) }
-            }
-        }
-    }
 
     extensions.findByType(KotlinMultiplatformExtension::class.java)?.run {
-        iosArm64("iosArm64", configureIos)
-        iosX64("iosX64", configureIos)
+        ios {
+            binaries {
+                framework("MultiPlatformLibrary") {
+                    freeCompilerArgs += "-Xobjc-generics"
+
+                    exports.forEach { it.export(project, this) }
+                }
+            }
+        }
     }
 }
