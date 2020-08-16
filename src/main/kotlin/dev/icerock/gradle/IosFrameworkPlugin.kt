a40b211f6e523c5641355704d5c2d753b961ede5
Aleksey Mikhailov <Aleksey.Mikhailov@icerockdev.com>
2020-08-16 21:51:53 +0700

    replace setupFramework to plugin ios-framework

---
+++ src/main/kotlin/dev/icerock/gradle/IosFrameworkPlugin.kt
@@ -0,0 +1,26 @@
+/*
+ * Copyright 2020 IceRock MAG Inc. Use of this source code is governed by the Apache 2.0 license.
+ */
+
+package dev.icerock.gradle
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
+
+class IosFrameworkPlugin : Plugin<Project> {
+    override fun apply(target: Project) {
+        val frameworkExtension = target.extensions.create("framework", FrameworkConfig::class.java)
+        val kmpExtension = target.extensions.findByType(KotlinMultiplatformExtension::class.java) ?: return
+
+        kmpExtension.ios {
+            binaries {
+                framework(frameworkExtension.name) {
+                    freeCompilerArgs += "-Xobjc-generics"
+
+                    frameworkExtension.exports?.forEach { it.export(project, this) }
+                }
+            }
+        }
+    }
+}
