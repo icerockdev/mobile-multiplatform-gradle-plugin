846d067f8db547e6570518b850eaf3888406d7d1
Aleksey Mikhailov <Aleksey.Mikhailov@icerockdev.com>
2020-08-16 21:43:46 +0700

    add gradle plugins extensions

---
+++ src/main/kotlin/GradlePlugin.kt
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2020 IceRock MAG Inc. Use of this source code is governed by the Apache 2.0 license.
+ */
+
+import org.gradle.api.artifacts.Dependency
+import org.gradle.kotlin.dsl.DependencyHandlerScope
+import org.gradle.plugin.use.PluginDependenciesSpec
+import org.gradle.plugin.use.PluginDependencySpec
+
+data class GradlePlugin(
+    val id: String,
+    val module: String? = null,
+    val version: String? = null
+)
+
+fun DependencyHandlerScope.plugin(gradlePlugin: GradlePlugin): Dependency? {
+    return gradlePlugin.module?.let { "classpath"(it) }
+}
+
+fun DependencyHandlerScope.plugins(gradlePluginList: List<GradlePlugin>) {
+    gradlePluginList
+        .distinctBy { it.module }
+        .forEach { plugin(it) }
+}
+
+fun PluginDependenciesSpec.plugin(gradlePlugin: GradlePlugin): PluginDependencySpec {
+    val spec = id(gradlePlugin.id)
+    gradlePlugin.version?.also { spec.version(it) }
+    return spec
+}
