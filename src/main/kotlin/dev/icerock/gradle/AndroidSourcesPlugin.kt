475cf44a00daa1263029591482361f4c27a469d3
Aleksey Mikhailov <Aleksey.Mikhailov@icerockdev.com>
2021-06-26 13:59:13 +0700

    #50 add android sources plugin

---
+++ src/main/kotlin/dev/icerock/gradle/AndroidSourcesPlugin.kt
@@ -0,0 +1,25 @@
+/*
+ * Copyright 2019 IceRock MAG Inc. Use of this source code is governed by the Apache 2.0 license.
+ */
+
+package dev.icerock.gradle
+
+import com.android.build.gradle.LibraryExtension
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class AndroidSourcesPlugin : Plugin<Project> {
+    override fun apply(target: Project) {
+        target.plugins.withId("com.android.library") {
+            val androidExtension = target.extensions.findByType(LibraryExtension::class.java)!!
+
+            androidExtension.sourceSets.configureEach {
+                val capitalizedName = name.capitalize()
+                val newRoot = "src/android$capitalizedName"
+                setRoot(newRoot)
+
+                target.logger.debug("set new root for $name android source set - $newRoot")
+            }
+        }
+    }
+}
